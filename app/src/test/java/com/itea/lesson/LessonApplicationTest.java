d57c43e5e73528ee8f7d0f650a51badd9e03118b
vadimkononenko <vadiskon@gmail.com>
2020-02-08 12:16:43 +0200

    feature: create microservices

---
+++ app/src/test/java/com/itea/lesson/LessonApplicationTest.java
@@ -0,0 +1,13 @@
+package com.itea.lesson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = LessonApplication.class)
+public class LessonApplicationTest {
+
+    @Test
+    void contextLoads() {
+
+    }
+}
